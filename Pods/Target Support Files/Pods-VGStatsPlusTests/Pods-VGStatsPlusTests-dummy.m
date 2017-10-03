78dba50bceb0ef70d29a5a56abc936b903b5039f
Tennant Shaw <ctshaw325@gmail.com>
2017-10-03 09:54:23 -0400

    added swift wrapper for Vainglory API

---
+++ Pods/Target Support Files/Pods-VGStatsPlusTests/Pods-VGStatsPlusTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VGStatsPlusTests : NSObject
+@end
+@implementation PodsDummy_Pods_VGStatsPlusTests
+@end
--- VGStatsPlus/VainGloryAPI.swift
+++ VGStatsPlus/VainGloryAPI.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import VaingloryAPI
 
 enum RegionalShards: String {
     case northAmerica = "na"
