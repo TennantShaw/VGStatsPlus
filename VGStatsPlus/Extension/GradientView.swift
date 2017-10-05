64cc4644a8444bfb05fc8397a04f0fe0d844b11e
MrAli•Gorithm <alisher.coder@gmail.com>
2017-10-05 17:49:18 -0400

    PlayerInfoControllerUpdated
    Here is how we can use the data we have pulled

---
+++ VGStatsPlus/Extension/GradientView.swift
@@ -0,0 +1,9 @@
+//
+//  GradientView.swift
+//  VGStatsPlus
+//
+//  Created by Alisher Abdukarimov on 10/5/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import Foundation
---
+++ VGStatsPlus/Extension/UIImageViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageViewExtension.swift
+//  VGStatsPlus
+//
+//  Created by Alisher Abdukarimov on 10/5/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import Foundation
