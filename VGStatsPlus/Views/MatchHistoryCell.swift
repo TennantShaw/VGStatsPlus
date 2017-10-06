ddcb7ca87c797e670e91b08154f51951e6fb5a97
Tennant Shaw <ctshaw325@gmail.com>
2017-10-06 11:33:47 -0400

    trying to figure out an error

---
+++ VGStatsPlus/Views/MatchHistoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MatchHistoryCell.swift
+//  VGStatsPlus
+//
+//  Created by Tennant Shaw on 10/6/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import UIKit
+import VaingloryAPI
+
+class MatchHistoryCell: UITableViewCell {
+    
+}
