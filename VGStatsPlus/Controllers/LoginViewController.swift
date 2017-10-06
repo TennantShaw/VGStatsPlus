dec32385d1b4244bd2f69dd5d12a427d1c7d41ba
Tennant Shaw <ctshaw325@gmail.com>
2017-10-06 08:43:10 -0400

    adjusted func to use textField.text and removed tapGestureRecognizer

--- VGStatsPlus/Controllers/LoginViewController.swift
+++ VGStatsPlus/Controllers/LoginViewController.swift
@@ -19,7 +19,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: - Actions
     @IBAction func submitButton(_ sender: UIButton) {
-        if textFieldIGN.text != "" { VGDataSource.instance.getUserData(name: "TennantTheVast", regional: "na", success: { (success) in
+        let ignTextField = textFieldIGN.text!
+        let shardTextField = regionShardTextField.text!
+        if textFieldIGN.text != "" { VGDataSource.instance.getUserData(name: ignTextField, regional: shardTextField, success: { (success) in
             if success {
                 print("successFully got data")
             }
@@ -27,21 +29,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         } else {
             // let user know that they cannot submit until they have added an IGN and selected a server region
         }
-        
-        let filters = RouterFilters()
-            .playerName(textFieldIGN.text!)
-            .limit(5)
-
-        vainGloryAPI.getMatches(shard: Shard(rawValue: playerRegionShard!)!, filters: filters) { matches, error in
-            if let matches = matches {
-                for match in matches {
-                    print("[VaingloryAPI] \(match)")
-                }
-            } else if let error = error {
-                    print("[VaingloryAPI] \(error)")
-                }
-            }
-            
+                
         }
     
     
