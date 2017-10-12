82ab500c6fdd297cb4d0878015b45ddb82e5a989
Tennant Shaw <ctshaw325@gmail.com>
2017-10-12 17:03:15 -0400

    added and connected outlets for matchHistoryCell

--- VGStatsPlus/Views/MatchHistoryCell.swift
+++ VGStatsPlus/Views/MatchHistoryCell.swift
@@ -13,6 +13,25 @@ class MatchHistoryCell: UITableViewCell {
     //MARK: - Outlets
     @IBOutlet var leftSideCharacterImageView: UIImageView!
     @IBOutlet var rightSideCharacterImageView: UIImageView!
+    @IBOutlet var leftSideItemOneImageView: UIImageView!
+    @IBOutlet var rightSideItemOneImageView: UIImageView!
+    @IBOutlet var leftSideItemTwoImageView: UIImageView!
+    @IBOutlet var rightSideItemTwoImageView: UIImageView!
+    @IBOutlet var leftSideItemThreeImageView: UIImageView!
+    @IBOutlet var rightSideItemThreeImageView: UIImageView!
+    @IBOutlet var leftSideItemFourImageView: UIImageView!
+    @IBOutlet var rightSideItemFourImageView: UIImageView!
+    @IBOutlet var leftSideItemFiveImageView: UIImageView!
+    @IBOutlet var rightSideItemFiveImageView: UIImageView!
+    @IBOutlet var leftSideItemSixImageView: UIImageView!
+    @IBOutlet var rightSideItemSixImageView: UIImageView!
+
+    @IBOutlet var leftSideKDALabel: UILabel!
+    @IBOutlet var rightSideKDALabel: UILabel!
+    @IBOutlet var leftSideCSLabel: UILabel!
+    @IBOutlet var rightSideCSLabel: UILabel!
+    @IBOutlet var leftSideGoldLabel: UILabel!
+    @IBOutlet var rightSideGoldLabel: UILabel!
     
     
     //MARK: - Nib
