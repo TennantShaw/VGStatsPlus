5d059b731869ddd39854253f402e6ffc88a19f65
Tennant Shaw <ctshaw325@gmail.com>
2017-10-12 21:03:23 -0400

    rafactored code for mulitple tableViews in ViewController

--- VGStatsPlus/Controllers/MatchHistoryViewController.swift
+++ VGStatsPlus/Controllers/MatchHistoryViewController.swift
@@ -22,43 +22,45 @@ class MatchHistoryViewController: UIViewController, UINavigationControllerDelega
     @IBOutlet var leftSideVictoryOrDefeatLabel: UILabel!
     @IBOutlet var rightSideVictoryOrDefeatLabel: UILabel!
     
+    @IBOutlet var leftSideTableView: UITableView!
+    @IBOutlet var rightSideTableView: UITableView!
+    
     //MARK: - Properties
     var matchResource: MatchResource?
+    var matches: [MatchResource] = []
     
     //MARK: = View Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
-        
-        // side menu setup // Taking out until I know why it is allowing to go to previous viewController while chat side menu is open
-//        revealViewController().rightViewRevealWidth = self.view.frame.width / 3
-//        self.view.addGestureRecognizer(super.revealViewController().panGestureRecognizer())
-//        self.view.addGestureRecognizer(super.revealViewController().tapGestureRecognizer())
-//
-//        revealViewController().rightViewRevealWidth = self.view.frame.width - 50
-
     }
-    
-
 
 }
-
 
 extension MatchHistoryViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if tableView == leftSideTableView {
+            return 1
+        } else {
+            return 1
+        }
     }
     
     //MARK: - DataSource Methods
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "leftSideCell") as? MatchHistoryCell else { return UITableViewCell() }
-        return cell
+        if tableView == leftSideTableView {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "leftSideCell") as? LeftSideCell else { return UITableViewCell() }
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "rightSideCell") as? RightSideCell else { return UITableViewCell() }
+            return cell
+        }
     }
+    
 }
 
--- VGStatsPlus/Views/MatchHistoryCell.swift
+++ VGStatsPlus/Views/MatchHistoryCell.swift
@@ -9,41 +9,25 @@
 import UIKit
 import VaingloryAPI
 
-class MatchHistoryCell: UITableViewCell {
+class LeftSideCell: UITableViewCell {
     //MARK: - Outlets
     @IBOutlet var leftSideCharacterImageView: UIImageView!
-    @IBOutlet var rightSideCharacterImageView: UIImageView!
     @IBOutlet var leftSideItemOneImageView: UIImageView!
-    @IBOutlet var rightSideItemOneImageView: UIImageView!
     @IBOutlet var leftSideItemTwoImageView: UIImageView!
-    @IBOutlet var rightSideItemTwoImageView: UIImageView!
     @IBOutlet var leftSideItemThreeImageView: UIImageView!
-    @IBOutlet var rightSideItemThreeImageView: UIImageView!
     @IBOutlet var leftSideItemFourImageView: UIImageView!
-    @IBOutlet var rightSideItemFourImageView: UIImageView!
     @IBOutlet var leftSideItemFiveImageView: UIImageView!
-    @IBOutlet var rightSideItemFiveImageView: UIImageView!
     @IBOutlet var leftSideItemSixImageView: UIImageView!
-    @IBOutlet var rightSideItemSixImageView: UIImageView!
 
     @IBOutlet var leftSideKDALabel: UILabel!
-    @IBOutlet var rightSideKDALabel: UILabel!
     @IBOutlet var leftSideCSLabel: UILabel!
-    @IBOutlet var rightSideCSLabel: UILabel!
     @IBOutlet var leftSideGoldLabel: UILabel!
-    @IBOutlet var rightSideGoldLabel: UILabel!
     
     
     //MARK: - Nib
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     
@@ -52,3 +36,30 @@ class MatchHistoryCell: UITableViewCell {
     }
 
 }
+
+class RightSideCell: UITableViewCell {
+    //MARK: - Outlets
+    @IBOutlet var rightSideCharacterImageView: UIImageView!
+    @IBOutlet var rightSideItemOneImageView: UIImageView!
+    @IBOutlet var rightSideItemFiveImageView: UIImageView!
+    @IBOutlet var rightSideItemFourImageView: UIImageView!
+    @IBOutlet var rightSideItemThreeImageView: UIImageView!
+    @IBOutlet var rightSideItemSixImageView: UIImageView!
+    @IBOutlet var rightSideItemTwoImageView: UIImageView!
+    
+    @IBOutlet var rightSideKDALabel: UILabel!
+    @IBOutlet var rightSideCSLabel: UILabel!
+    @IBOutlet var rightSideGoldLabel: UILabel!
+    
+    //MARK: - Nib
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    //MARK: - Class Methods
+    func setupCell(match: MatchResource?) {
+    }
+}
+
