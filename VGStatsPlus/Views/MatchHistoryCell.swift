7666b1cabe8d39240402edc6f334c72c9191b04c
Tennant Shaw <ctshaw325@gmail.com>
2017-11-02 13:14:14 -0400

    updated left and right tableviews with correct label data
    still need to add new images into asset file

--- VGStatsPlus/Views/MatchHistoryCell.swift
+++ VGStatsPlus/Views/MatchHistoryCell.swift
@@ -19,6 +19,8 @@ class LeftSideCell: UITableViewCell {
     @IBOutlet var leftSideItemFiveImageView: UIImageView!
     @IBOutlet var leftSideItemSixImageView: UIImageView!
 
+    
+    @IBOutlet weak var leftSidePlayerNameLabel: UILabel!
     @IBOutlet var leftSideKDALabel: UILabel!
     @IBOutlet var leftSideCSLabel: UILabel!
     @IBOutlet var leftSideGoldLabel: UILabel!
@@ -33,7 +35,40 @@ class LeftSideCell: UITableViewCell {
     
     //MARK: - Class Methods
     func setupCell(participant: ParticipantResource?) {
+        guard participant != nil else { return }
+        let player = participant!.player
+        
+        leftSideCharacterImageView.image = UIImage(named: "\(participant!.actor!.description)")
+        
+        if participant!.items!.count == 1 {
+            leftSideItemOneImageView.image = UIImage(named: "\(participant!.items![0].description)")
+        }
+        if participant!.items!.count == 2 {
+            leftSideItemTwoImageView.image = UIImage(named: "\(participant!.items![1].description)")
+        }
+        if participant!.items!.count == 3 {
+            leftSideItemThreeImageView.image = UIImage(named: "\(participant!.items![2].description)")
+        }
+        if participant!.items!.count == 4 {
+            leftSideItemFourImageView.image = UIImage(named: "\(participant!.items![3].description)")
+        }
+        if participant!.items!.count == 5 {
+            leftSideItemFiveImageView.image = UIImage(named: "\(participant!.items![4].description)")
+        }
+        if participant!.items!.count == 6 {
+            leftSideItemSixImageView.image = UIImage(named: "\(participant!.items![5].description)")
+        }
+        
+        leftSidePlayerNameLabel.text = "\(player!.name!.description)"
+        leftSideKDALabel.text = "\(participant!.kills!.description) / \(participant!.deaths!.description) / \(participant!.assists!.description)"
+        leftSideCSLabel.text = "\(participant!.minionKills!.description)"
+        leftSideGoldLabel.text = "\(participant!.gold!.description)"
+        
+        print("\n")
+        print("----------Left")
         print(participant)
+        print("----------Left")
+        print("\n")
     }
 }
 
@@ -47,6 +82,8 @@ class RightSideCell: UITableViewCell {
     @IBOutlet var rightSideItemSixImageView: UIImageView!
     @IBOutlet var rightSideItemTwoImageView: UIImageView!
     
+    
+    @IBOutlet weak var rightSidePlayerNameLabel: UILabel!
     @IBOutlet var rightSideKDALabel: UILabel!
     @IBOutlet var rightSideCSLabel: UILabel!
     @IBOutlet var rightSideGoldLabel: UILabel!
@@ -60,7 +97,40 @@ class RightSideCell: UITableViewCell {
     
     //MARK: - Class Methods
     func setupCell(participant: ParticipantResource?) {
+        guard participant != nil else { return }
+        let player = participant!.player
+        
+        rightSideCharacterImageView.image = UIImage(named: "\(participant!.actor!.description)")
+        
+        if participant!.items!.count == 1 {
+            rightSideItemOneImageView.image = UIImage(named: "\(participant!.items![0].description)")
+        }
+        if participant!.items!.count == 2 {
+            rightSideItemTwoImageView.image = UIImage(named: "\(participant!.items![1].description)")
+        }
+        if participant!.items!.count == 3 {
+            rightSideItemThreeImageView.image = UIImage(named: "\(participant!.items![2].description)")
+        }
+        if participant!.items!.count == 4 {
+            rightSideItemFourImageView.image = UIImage(named: "\(participant!.items![3].description)")
+        }
+        if participant!.items!.count == 5 {
+            rightSideItemFiveImageView.image = UIImage(named: "\(participant!.items![4].description)")
+        }
+        if participant!.items!.count == 6 {
+            rightSideItemSixImageView.image = UIImage(named: "\(participant!.items![5].description)")
+        }
+        
+        rightSidePlayerNameLabel.text = "\(player!.name!.description)"
+        rightSideKDALabel.text = "\(participant!.kills!.description) / \(participant!.deaths!.description) / \(participant!.assists!.description)"
+        rightSideCSLabel.text = "\(participant!.minionKills!.description)"
+        rightSideGoldLabel.text = "\(participant!.gold!.description)"
+
+        print("\n")
+        print("----------Right")
         print(participant)
+        print("----------Right")
+        print("\n")
     }
     
 }
