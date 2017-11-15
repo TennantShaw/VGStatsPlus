5bc9496e7f5ee271cb083857f3c279743704bcdc
MrAli•Gorithm <alisher.coder@gmail.com>
2017-11-15 15:17:04 -0600

    BeforeMerge

--- VGStatsPlus/Controllers/MenuViewController.swift
+++ VGStatsPlus/Controllers/MenuViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 import Firebase
 
 class MenuViewController: UIViewController {
+    
+    @IBOutlet var userProfileImage: CircleImage!
+    @IBOutlet var nameLabel: UILabel!
+    
+    
     //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.width - 50
+        VGFirebaseDB.instance.getUserInfo(withID: SavedStatus.instance.userID) { (name, image) in
+          //  self.userImageView.downloadedFrom(link: image)
+        }
     }
 
     @IBAction func handleLogin(_ sender: Any) {
